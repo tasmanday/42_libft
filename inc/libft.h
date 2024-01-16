8c9ed57129c907cff3220d86383db88358935b87
Tasman Day <122511599+tasmanday@users.noreply.github.com>
2024-01-16 23:22:58 +1030

    added conversion functions

--- inc/libft.h
+++ inc/libft.h
@@ -17,6 +17,7 @@
 # include <unistd.h>
 # include <stdio.h>
 # include <stdarg.h>
+# include <limits.h>
 
 /*
 ** structs
@@ -44,48 +45,48 @@ typedef struct s_dlist
 */
 
 /* character */
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_isprint(int c);
-int		ft_isspace(int c);
-int		ft_toupper(int c);
-int		ft_tolower(int c);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
+int			ft_isspace(int c);
+int			ft_toupper(int c);
+int			ft_tolower(int c);
 
 /* put */
 
-void	ft_putchar(char c);
-void	ft_putstr(char *str);
-void	ft_putnbr(int nb);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putstr_fd(char *str, int fd);
-void	ft_putendl_fd(char *str, int fd);
-void	ft_putnbr_fd(int n, int fd);
+void		ft_putchar(char c);
+void		ft_putstr(char *str);
+void		ft_putnbr(int nb);
+void		ft_putchar_fd(char c, int fd);
+void		ft_putstr_fd(char *str, int fd);
+void		ft_putendl_fd(char *str, int fd);
+void		ft_putnbr_fd(int n, int fd);
 
 /* ft_printf */
 
-void	ft_character(char c, int *length);
-void	ft_string(char *s, int *length);
-void	ft_decimal_integer(int d, int *length);
-void	ft_unsigned_integer(unsigned int u, int *length);
-void	ft_hexidecimal(unsigned int x, int *length, char char_case, int first);
-void	ft_pointer(size_t p, int *length, int first);
-int		ft_printf(const char *string, ...);
+void		ft_character(char c, int *length);
+void		ft_string(char *s, int *length);
+void		ft_decimal_integer(int d, int *length);
+void		ft_unsigned_integer(unsigned int u, int *length);
+void		ft_hexidecimal(unsigned int x, int *length, char char_case, int first);
+void		ft_pointer(size_t p, int *length, int first);
+int			ft_printf(const char *string, ...);
 
 /* memory */
 
-void	*ft_memset(void *pointer, int c, size_t n);
-void	ft_bzero(void *pointer, size_t n);
-void	*ft_memcpy(void *dest, const void *src, size_t n);
-void	*ft_memmove(void *dest, const void *src, size_t n);
-void	*ft_memchr(const void *ptr, int c, size_t n);
-int		ft_memcmp(const void *s1, const void *s2, size_t n);
-void	*ft_calloc(size_t number, size_t size);
-void	*ft_realloc(void *pointer, size_t orig_size, size_t new_size);
+void		*ft_memset(void *pointer, int c, size_t n);
+void		ft_bzero(void *pointer, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memchr(const void *ptr, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
+void		*ft_calloc(size_t number, size_t size);
+void		*ft_realloc(void *pointer, size_t orig_size, size_t new_size);
 
 /* conversion */
-
-
+long long	ft_atoi(const char *str);
+char		*ft_itoa(long long n);
 
 #endif
